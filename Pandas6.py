ae2b2a68cb3593b200c8a645dad94b573961901a
MrYokkie <63582113+MrYokkie@users.noreply.github.com>
2022-01-19 21:49:17 +0100

    Update Pandas6.py

--- Pandas6.py
+++ Pandas6.py
@@ -6,23 +6,20 @@ df["Residence"].value_counts()
 df["Citizenship"].value_counts()
 polacy = df[df["Citizenship"] == "Poland"]#poisk i wkidywanie do peremennoj nuznuju nam kolonu
 for index, row in df.iloc[:3,:].iterrows():
-   #wytiagiwanie kazdogo wiersza po oczeregi czto by ich razdelit, sdelat czto-to s kazdym
    print("Index to", index)
    print("Row to:", row)
 
 for index, row in df.iterrows():
-   #wytiagiwanie kazdogo wiersza po oczeregi czto by ich razdelit, sdelat czto-to s kazdym
    print("Index to", index)
    print("Row to:", row)
 
 for index, row in df.iterrows():
-   #wytiagiwanie kazdogo wiersza po oczeregi czto by ich razdelit, sdelat czto-to s kazdym
    if row.Citizenship == "Poland":
      print("Yes")
 
 df.loc[polacy,["Name","NetWorth"]]
 df[df.Citizenship == df.Residence]
-df.Status.value_counts()
+df.Status.value_counts() #dict ctreating for replacement 
 sl = {
     "Married":"yes",
     "Divorced":"no",
@@ -34,26 +31,26 @@ sl = {
     "Engaged":"yes"
 }
 
-df.Status.replace(sl)# peremestili peremennyje s kolon w slowar
-def vc(s): # funkcja kotoraja ubirajet braki danych
+df.Status.replace(sl)
+def vc(s): # function to drop all NaN in data
   return s.value_counts(dropna = False)
 vc(df.Status)
 df["Relationship"] = df.Status.replace(sl)
 vc(df.Relationship)
 a
 b = ["yes","no","no","no", "yes","no","yes","yes"]
-dict(zip(a,b)) # funkcja zip skleiwajet neskolko list w python
-#paczka pingouin dla statistiki w python
+dict(zip(a,b)) #Due to zip operator we can compile 2 seriases in df
+
 !pip install pingouin
 df.NetWorth
-png.corr(df.NetWorth, df.Age)# poisk korelacji mezdu czemto a czemto
+png.corr(df.NetWorth, df.Age) #correlations reasearch between features
 self_made = (df.Self_made == "True")
 df.Self_made
 png.corr(df[self_made].NetWorth, df[self_made].Age)
-df.Children == 0 # poisk braka danych
+df.Children == 0 # search gaps in data/ also can be realized via comand df.Children.isnull().sum()
 df.Children == 1 
 b = (df.loc[df["Children"] >= 1,"NetWorth"])
 c = (df.loc[df["Children"].is_na(), "NetWorth"])
-png.ttest(dzietni, bezdzietni) #t test dla detnych i bezdetnych
+png.ttest(dzietni, bezdzietni) #t test for 2 fetures
 png.chi2_independence(df, "Relationship","Self_made")
 e,o png.chi2_independence(df, "Relationship","Self_made")
