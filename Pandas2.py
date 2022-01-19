f6b121699ba017d5ee59d4255e706015b4fe352c
MrYokkie <63582113+MrYokkie@users.noreply.github.com>
2022-01-19 20:51:04 +0100

    Update Pandas2.py

--- Pandas2.py
+++ Pandas2.py
@@ -1,33 +1,33 @@
-import pandas as pd
-data = pd.read_csv("Trial.csv")#import dokumenta
-data#print dokumenta
-data.columns#funkcja cztoby uznat kakie kolonu w tablice
-import matplotlib.pyplot as plt #import paketa dla postrojenia grafika
-plt.plot(data['id'], data['wiek'])#postroenie linejnogo grafika gde x = id, y = wozrast
-plt.hist(data['wiek']) #gistrograma dla wozrasta
-trial = pd.read_excel("Trial.xlsx")#jesli mnogo sheets w faile to dobawit "sheet name = ..."
+import pandas as pd #Import Pandas
+data = pd.read_csv("Trial.csv") #import csv file
+data
+data.columns
+import matplotlib.pyplot as plt #import matplotlib
+plt.plot(data['id'], data['wiek'])
+plt.hist(data['wiek']) 
+trial = pd.read_excel("Trial.xlsx")
 trial
 type(trial)
-a = trial["wiek"] #wzal odtelnyje kolony tablic i peredelal tablicu ta kotoraja mnie nuzna
+a = trial["wiek"] #extract a series for df creation
 b = trial["id"]
 data2 = pd.DataFrame ({
     "Year": a,
     "id": b
 })
-trial2 = pd.read_excel("Trial.xlsx", index_col= "id")#izbawitsa ot numeracji w tablice
+trial2 = pd.read_excel("Trial.xlsx", index_col= "id")
 trial2
-trial2["wiek"][19]#poisk po indeksam
+trial2["wiek"][19]
 data2
-id_pacjenta = 13 #poisk pacjenta po nomeru identifikatora i poisk wyzyl li on ili net
+id_pacjenta = 13 #search of patient number 13
 if trial2["zgon"][id_pacjenta] == "tak":
   print(trial2["interwencja"][id_pacjenta])
-#ispolzowanie funkcji loc/iloc
-trial.wiek #segragacja wozrasta, filtr toj kolony kotoraja nas interesujet
-trial.zgon.value_counts()#podszczot skolko wyzylo a skolko net
-trial.loc[0,"wiek"]#najpierw nazwa obserwacji/mozem filtrowat danyje
+#loc/iloc functions use
+trial.wiek 
+trial.zgon.value_counts()
+trial.loc[0,"wiek"]
 trial.loc[:5, "wiek"] 
-trial.loc[:5,["wiek","id"]] #perwyje 5 pointow obserawcji
-trial.loc[10,["interwencja","zgon"]] #10j point, wytianuli nuznuju infu po niem
+trial.loc[:5,["wiek","id"]] 
+trial.loc[10,["interwencja","zgon"]] 
 trial.loc[4:10:2,["interwencja","zgon"]]
 trial.loc[5:10, "wiek":"zgon"]
 trial.loc[trial.zgon == "tak","wiek"]
