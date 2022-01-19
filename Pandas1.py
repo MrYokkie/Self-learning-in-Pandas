71e2deffc1c21539d3e45ae7cffe0e5148beb932
MrYokkie <63582113+MrYokkie@users.noreply.github.com>
2022-01-19 20:47:50 +0100

    Update Pandas1.py

--- Pandas1.py
+++ Pandas1.py
@@ -1,12 +1,12 @@
-#import neobchodimych paketow
+#import of nescessary packages
 import pandas as pd
 import matplotlib.pyplot as plt
-#sozdanie list dla tablicy (daliee DataFrame)
+#within first lessone as a homework DataFrame has been created
 a = (12523,32525,236264,96454234,124719820)
 b = ("Ukrain","Poland","Germany","USA","China")
 c = (2019)
 d = (42000000,32000000, 83000000,328000000,170000000)
-#Pokaz wsech listow
+#To check all lists applied for dataframe creating
 print(a)
 print(b)
 #sozdanie tablicy pri ispolzowanii funkcjii n = pd.DataFrame({})
@@ -18,7 +18,7 @@ df = pd.DataFrame({
 })
 
 print(df)
-#osnownyje danyje otnositelno tablicy
+#extract all info tpwards to df
 df.describe()
-#postrojka linejnogo grafika
+#plotting a lineplot with Pandas
 plt.plot(df['Country'],df["GDP"])
